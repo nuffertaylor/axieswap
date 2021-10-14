3018d641891c3a8a29be06e3b916cb375f13dfa8
Taylor Nuffer <tnuffer@taylors-air.lan>
2021-10-13 21:42:52 -0600

    created deposit files

---
+++ scripts/deposit.js
@@ -0,0 +1,3 @@
+//TODO: populate these boxes here instead of statically above. 
+//TODO: adjust so when an axie is clicked they are moved from one box to the other. 
+//TODO: Allow the percentage to be changed and saved on change.
