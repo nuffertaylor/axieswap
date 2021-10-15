f589479dfbf1c433a0ae77f4d73591aa8383b655
Taylor Nuffer <tnuffer@taylors-air.lan>
2021-10-15 11:28:42 -0600

    added deposit button functionality

--- scripts/deposit.js
+++ scripts/deposit.js
@@ -89,7 +89,24 @@ function updateAxieRate(axieID, rate)
 function depositAxies()
 {
   //verify there are axies to deposit
+  if(axiesToDeposit.length == 0)
+  {
+    alert("You must select Axies before you can deposit them!");
+    return;
+  }
+  let axieStr = " axie";
+  if(axiesToDeposit.length > 1) axieStr = axieStr + "s";
   //send message to verify user wishes to deposit
-  //push confirmation of transaction to wallet
-  //remove axies
+  if(confirm("Are you sure you want to deposit " + axiesToDeposit.length + axieStr + "?"))
+  {
+    //push confirmation of transaction to wallet
+    //we'll simulate this by waiting 5 seconds before removing the axies
+    setTimeout(() => {  
+      //remove axies
+      axiesToDeposit = [];
+      populateContainers();
+      //notify user of success
+      alert("successfully deposited your" + axieStr);
+      }, 5000);
+  }
 }
